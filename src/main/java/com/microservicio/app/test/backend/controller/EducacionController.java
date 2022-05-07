7242009588536623b07a8a4ed6bc57a2b0c0ee8c
jimmy palomino <jimmy_13_5@hotmail.com>
2022-05-07 18:47:43 -0300

    Se agrego el recurso de adregar una educacion

--- src/main/java/com/microservicio/app/test/backend/controller/EducacionController.java
+++ src/main/java/com/microservicio/app/test/backend/controller/EducacionController.java
@@ -1,17 +1,21 @@
 package com.microservicio.app.test.backend.controller;
 
+import com.microservicio.app.test.backend.dto.EducacionCrearDto;
 import com.microservicio.app.test.backend.dto.EducacionDto;
 import com.microservicio.app.test.backend.entity.Educacion;
+import com.microservicio.app.test.backend.exception.InvalidDataException;
 import com.microservicio.app.test.backend.service.EducacionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class EducacionController {
@@ -29,6 +33,17 @@ public class EducacionController {
     public ResponseEntity<Educacion> buscarEducacion(@PathVariable long id){
 
         return ResponseEntity.ok(educacionService.educacion(id));
+    }
+
+    @PostMapping("/agregar-educacion")
+    public ResponseEntity<EducacionDto> agregarEducacion(@Valid @RequestBody EducacionCrearDto educacionCrearDto, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()) {
+            throw new InvalidDataException(bindingResult);
+        }
+
+        log.info("Creando tecnologia " + educacionCrearDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(educacionService.addEducacion(educacionCrearDto));
     }
 
 
